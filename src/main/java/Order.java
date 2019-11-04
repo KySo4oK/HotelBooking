df50de5ac1cfb0c8fdf56cc23454afba46410f0c
KySo4oK <prostorostuk08@gmail.com>
2019-11-04 21:41:27 +0200

    edit variable

--- src/main/java/Order.java
+++ src/main/java/Order.java
@@ -24,7 +24,7 @@ public class Order {
     @JoinColumn(name = "user_id")
     private User user;
     @ManyToMany
-    private Set<AdditionalOption> AdditionalOptions;
+    private Set<AdditionalOption> additionalOptions;
     @Column(name = "startDate")
     private Date startDate;
     @Column(name = "endDate")
