75533ec2cc967d5cbd4478a4eb1ba6a7c9d776d5
KySo4oK <prostorostuk08@gmail.com>
2020-01-02 00:59:45 +0200

    first try to fix old methods

--- src/main/java/Hotel.java
+++ src/main/java/Hotel.java
@@ -8,23 +8,15 @@ import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
 
 @Setter
 @Getter
 
 
-
-public class Hotel implements MyService{
+public class Hotel implements MyService {
     private static EntityManagerFactory emf = Persistence.createEntityManagerFactory("PERSISTENCE_UNIT_NAME");
     private static EntityManager em = getEntityManager();
-
-    @Override
-    public void viewRoomsByCategory(Category category) {
-
-    }
 
     @Override
     public void book(Date startDate, Date endDate) {
@@ -61,62 +53,65 @@ public class Hotel implements MyService{
         return emf.createEntityManager();
     }
 
-    public void book(Date startDate, Date endDate){
-        ArrayList<Room> availableRooms = getRooms();
-        for(int i = 0; i < availableRooms.size(); i++){
-            if(!checkDates(availableRooms.get(i),startDate,endDate)){
+    public void book(Date startDate, Date endDate, User currentUser) {
+        List<Room> availableRooms = getAllRooms();
+        for (int i = 0; i < availableRooms.size(); i++) {
+            if (!checkDates(availableRooms.get(i), startDate, endDate)) {
                 availableRooms.remove(availableRooms.get(i));
             }
         }
         Room room = chooseRoom(availableRooms);
         Order order = new Order();// need add variables
-        ArrayList<Order> userOrders = currentUser.getUserOrders();
+        Set<Order> userOrders = currentUser.getOrders();
         userOrders.add(order);
-        currentUser.setUserOrders(userOrders);
+        currentUser.setOrders(userOrders);
         addBook(order);
     }
 
-    private Room chooseRoom(ArrayList<Room> availableRooms){// need to rewrite
+    private Room chooseRoom(List<Room> availableRooms) {// need to rewrite
         return null;
     }
 
-    private void viewBookings(){ //show all bookings, need to rewrite in next stage
-        ArrayList<Order> userOrders = currentUser.getUserOrders();
-        for(int i = 0; i < userOrders.size(); i++){
-            System.out.println(userOrders.get(i));
+    private void viewBookings(User currentUser) { //show all bookings, need to rewrite in next stage
+        Set<Order> userOrders = currentUser.getOrders();
+        Object[] userOrdersArray = userOrders.toArray();
+        for (int i = 0; i < userOrdersArray.length; i++) {
+            System.out.println(userOrdersArray[i]);
         }
     }
 
-    private boolean checkDates(Room room, Date startDate, Date endDate){
-        ArrayList<Order> bookings= room.getBookings();
-        for(int i = 0; i < bookings.size(); i++){
-            Order book = bookings.get(i);
-            if(!(book.getStartDate().getTime() < startDate.getTime()
-                    && book.getEndDate().getTime() < endDate.getTime())){
+    private boolean checkDates(Room room, Date startDate, Date endDate) {
+        Set<Order> bookings = room.getOrders();
+        for (Iterator<Order> it = bookings.iterator(); it.hasNext(); ) {
+            Order book = it.next();
+            if (!(book.getStartDate().getTime() < startDate.getTime()
+                    && book.getEndDate().getTime() < endDate.getTime())) {
                 return false;
             }
         }
         return true;
     }
 
-    public int getTotalPrice(Room currentRoom, Date startDate, Date endDate){
+    public int getTotalPrice(Room currentRoom, Date startDate, Date endDate, Set selectedAdditionalOptions) {
         int totalPrice = 0;
-        for(int i = 0; i < selectedAdditionalOptions.size(); i++){
-            totalPrice += selectedAdditionalOptions.get(i).getPrice();
+        for (Iterator<AdditionalOption> it = selectedAdditionalOptions.iterator(); it.hasNext(); ) {
+            totalPrice += it.next().getPrice();
         }
-        return totalPrice + currentRoom.getPrice()*(getQuantityOfDays(startDate,endDate));
+        return totalPrice + currentRoom.getPrice() * (getQuantityOfDays(startDate, endDate));
     }
 
     public int getQuantityOfDays(Date startDate, Date endDate) {
-        return (int)((endDate.getTime() -
-                startDate.getTime())/(1000*60*60*24));
+        return (int) ((endDate.getTime() -
+                startDate.getTime()) / (1000 * 60 * 60 * 24));
     }
 
-    public void addBook(Order order){
-        bookings.add(order);
+    public void addBook(Order order) {
+        em.getTransaction().begin();
+        em.merge(order);
+        em.getTransaction().commit();
     }
 
-    public void viewRoomsByCategory(Category category) {
-        category.getAvailableRooms();
+    public void viewRoomsByCategory(Category category) { // rewrite later
+        getAllRooms();
     }
 }
