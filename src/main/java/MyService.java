a76a0e09f6d241c0a1ef8dbfd0bf06300d27d1a2
KySo4oK <prostorostuk08@gmail.com>
2020-01-01 23:23:55 +0200

    change ArrayList to List in interface

--- src/main/java/MyService.java
+++ src/main/java/MyService.java
@@ -1,11 +1,12 @@
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 public interface MyService {
     void viewRoomsByCategory(Category category);
     void book(Date startDate, Date endDate);
     int getTotalPrice(Room currentRoom, Date startDate, Date endDate);
-    ArrayList<Room> getAllRooms();
-    ArrayList<Category> getAllCategories();
-    ArrayList<AdditionalOption> getAllAdditionalOptions();
+    List<Room> getAllRooms();
+    List<Category> getAllCategories();
+    List<AdditionalOption> getAllAdditionalOptions();
 }
